6918ad46bb2fdb6a8340dc77920ad72951690e7d
aspectx <gamernxt6@gmail.com>
2022-01-03 22:14:08 +0700

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,10 @@
 const { username } = require('../config/config.json')
-const colors = require('colors')
+require('colors')
 
 
 module.exports = {
     Login: function (msg) {
-        console.log(`[${username}] `.blue + "[Log In]".cyan + `: ${msg}`)
+        console.log(`[${username}] `.blue + "[Log In]".cyan + `   : ${msg}`)
     },
     db: function (msg) {
         console.log(`[${username}] `.blue + "[Data Base]".green + `: ${msg}`)
